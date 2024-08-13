2f10379b8197cb244612c86615ad4a724e275fd5
Ignacy Łątka <latkaignacy@gmail.com>
2024-08-13 09:47:56 +0200

    add revised nested lists example

--- example/src/empty/EmptyExample.tsx
+++ example/src/empty/EmptyExample.tsx
@@ -1,19 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import {
+  GestureHandlerRootView,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
 
-export default function EmptyExample() {
+export default function App() {
+  const renderItem = React.useCallback(() => {
+    return (
+      <View style={styles.root}>
+        <TouchableOpacity
+          style={styles.touchable}
+          onPress={() => console.log('TouchableOpacity onPress called')}>
+          <FlatList
+            horizontal={true}
+            pagingEnabled={true}
+            data={['red', 'green', 'blue']}
+            renderItem={(data) => {
+              return (
+                <View
+                  style={[{ backgroundColor: data.item }, styles.container]}
+                />
+              );
+            }}
+          />
+          <View>
+            <Text style={styles.title}>Title</Text>
+            <Text style={styles.subtitle}>Subtitle</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <GestureHandlerRootView>
+      <FlatList
+        data={[1, 2, 3, 4, 5]}
+        renderItem={renderItem}
+        contentContainerStyle={styles.list}
+      />
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    marginBottom: 20,
+  },
+  list: {
+    padding: 20,
+  },
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    width: 348,
+    height: 348,
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  subtitle: {
+    fontSize: 14,
+  },
+  touchable: {
+    height: 300,
+    width: 350,
+    borderWidth: 1,
+    borderColor: 'grey',
   },
 });
